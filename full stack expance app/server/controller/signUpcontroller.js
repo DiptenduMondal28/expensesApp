4a184ed3fc1ee8eb2a1ddf7b43de33f8912fb408
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-06-16 09:34:57 +0530

    F:add encryption for password and another

--- full stack expance app/server/controller/signUpcontroller.js
+++ full stack expance app/server/controller/signUpcontroller.js
@@ -1,4 +1,5 @@
 const Credential=require('../module/signupModule');
+const bcrypt=require('bcrypt');
 
 function isstringinvalid(string){
     if(string == undefined || string.length === 0){
@@ -9,44 +10,31 @@ function isstringinvalid(string){
 }
 
 module.exports.signup=async(req,res,next)=>{
-    const name=req.body.name;
-    const email=req.body.email;
-    const password=req.body.password;
-
+    const {name,email,password}=req.body;
     try{
         console.log(`name: ${name}  email:${email} password:${password}`);
         if(isstringinvalid(name) || isstringinvalid(email) || isstringinvalid(password)){
             return res.status(400).json({err:'bad parameters fill paramters'})
         }
-        const credential=await Credential.create({
-            name:name,
-            email:email,
-            password:password
-        }).then(()=>{
-            res.status(201).json({message:'successfully created a profile'})
+        bcrypt.hash(password,10,async(err,hash)=>{
+            console.log(err)
+            await Credential.create({
+                name,email,password:hash
+            }).then(()=>{
+                res.status(201).json({message:'successfully created a profile'})
+            })
         })
+        // const credential=await Credential.create({
+        //     name:name,
+        //     email:email,
+        //     password:password
+        // }).then(()=>{
+        //     res.status(201).json({message:'successfully created a profile'})
+        // })
         
     }catch(err){
        res.status(500).json(err)
     }
 }
 
-// module.exports.login=async(req,res,next)=>{
-//     // try{
-//         const email=req.body.email;
-//         const password=req.body.password;
-//         const credentialCheck=await Credential.findAll({where:{email}}).then(user =>{
-//             if(user.length>0){
-//                 if(user[0].password===password){
-//                     res.status(200).json({success:true,message:'user loggedin succesfull'})
-//                 }else{
-//                     res.status(400).json({success:false,message:'check password'})
-//                 }
-//             }else{
-//                 res.status(404).json({success:false,message:"user not signup"})
-//             }
-//         }).catch(err=>{
-//             res.status(500).json({success:false,message:err})
-//         })
-        
-// }
+
