417f6cc58c948fb21d6580d71df2b088bd14b21f
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-06-16 15:18:28 +0530

    File:chage file location for better visualization

--- full stack expance app/client/login/login.js
+++ full stack expance app/client/login/login.js
@@ -14,7 +14,7 @@ async function login(event){
         await axios.post('http://localhost:3000/login',detail).then(response=>{
             if(response.status===200){
                 alert(response.data.message)
-                window.location.href='./expenceTracker.html'
+                window.location.href='../website/expenceTracker.html'
             }else{
                 throw new Error(response.data.message)
             }
