cb833169186c9656b50b119f7333d375ef57c2d2
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-06-21 19:03:03 +0530

    U:update transaction method

--- full stack expance app/server/controller/controller.js
+++ full stack expance app/server/controller/controller.js
@@ -1,33 +1,37 @@
 const User=require('../module/module');
 const Credential=require('../module/signupModule')
-
+const sequelize=require('../util/database')
 
 module.exports.dataupload=async(req,res,next)=>{
     try{
+        const t = await sequelize.transaction()
         const name=req.body.name;
         const exp=req.body.exp;
         const item=req.body.item;
         const category=req.body.category;
-        console.log('datacreate')
-        //console.log(+name+exp+item+category);
-        console.log(req.user.id)
-        const userId=req.user.id;
-        User.create({
-            name:name,
-            exp:exp,
-            item:item,
-            category:category,
-            userId:req.user.id
-        }).then(result=>{
+        if(exp.length===0 || exp===undefined){
+            return res.status(400).json({success:false,message:'expence parameter missing'})
+        }
+        try{
+            await User.create({
+                name:name,
+                exp:exp,
+                item:item,
+                category:category,
+                userId:req.user.id
+            },{transaction: t})
+
             const totalExpence=Number(req.user.totalexpence)+Number(exp);
-            Credential.update({totalexpence:totalExpence},{where:{id:req.user.id}}).then(async()=>{
-                res.status(200).json({expence:expence})
-            }).catch(async(err)=>{
-                return res.status(500).json({success:false,error:err})
-            })
-        }).catch(err=>{
-            console.log(err)
-        })
+            await Credential.update({totalexpence:totalExpence},{where:{id:req.user.id},transaction: t})
+
+            await t.commit();
+            res.status(200).json({expence:expence})
+
+        }catch(err){
+            await t.rollback();
+            return res.status(500).json({success:false,error:err})
+        }
+       
     }catch(err){
         console.log(err);
     }
@@ -45,21 +49,46 @@ module.exports.getdata=async(req,res,next)=>{
 }
 
 module.exports.deletedata=async(req,res,next)=>{
+    const t = await sequelize.transaction()
     const id=req.params.id;
     const deleteid=await User.findByPk(id);
-    console.log(req.user.id)
+    console.log(deleteid)
+    console.log("deletd things expence:"+deleteid.exp)
+    console.log("delete request user id:"+req.user.id)
+    console.log("exp:"+req.user.totalexpence)
+    
     try{
-        const destruction=await deleteid.destroy({where:{userID:req.user.id}});
-        try{
-            res.send('deleted')
-        }catch(error){
-            res.send(error)
+        const deleteid=await User.findByPk(id);
+        if (!deleteid) {
+            return res.status(404).json({ success: false, message: 'Data not found' });
         }
+        console.log(deleteid)
+        console.log("deletd things expence:"+deleteid.exp)
+        console.log("delete request user id:"+req.user.id)
+        console.log("exp:"+req.user.totalexpence)
 
+        try{
+
+            await deleteid.destroy({where:{userID:req.user.id},transaction:t});
+            const totalExpence=Number(req.user.totalexpence)-Number(deleteid.exp);
+            await Credential.update({totalexpence:totalExpence},{where:{id:req.user.id},transaction: t});
+            await t.commit();
+            res.status(200).json({expence:expence})
+
+        }catch(err){
+
+            console.log(err)
+            await t.rollback();
+            return res.status(500).json({success:false,error:err})
+
+        }
+       
 
     }catch(err){
+
         console.log(err);
-        res.send(err);
+        return res.send(err);
+
     }
 }
 
